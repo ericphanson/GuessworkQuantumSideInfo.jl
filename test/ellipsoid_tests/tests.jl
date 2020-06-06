0249d04bc7e9214cbdc56df279402e2e946af0fc
Eric P. Hanson <5846501+ericphanson@users.noreply.github.com>
2020-06-07 00:08:21 +0100

    Add serialization test

--- test/ellipsoid_tests/tests.jl
+++ test/ellipsoid_tests/tests.jl
@@ -16,4 +16,24 @@ function nl_solver()
     with_optimizer(() -> EAGO.Optimizer(verbosity=0))
 end
 
+using GuessworkQuantumSideInfo: EllipsoidProblem
+using Serialization
+
+@testset "Serialization" begin
+    p = ones(2)/2
+    ρBs = [ randdm(2) for _ = 1:2]
+    prob = EllipsoidProblem(p, ρBs, nl_solver = nl_solver())
+    local sol, re_prob
+    mktemp() do path, io
+        serialize(path, prob)
+        sol = GuessworkQuantumSideInfo.ellipsoid_algorithm!(prob)
+        re_prob = deserialize(path)
+    end
+    re_sol = GuessworkQuantumSideInfo.ellipsoid_algorithm!(re_prob)
+    @test sol.prob.p ≈ re_sol.prob.p
+    @test sol.prob.ρBs ≈ re_sol.prob.ρBs
+    @test sol.optval ≈ re_sol.optval rtol=1e-2
+    @test sol.prob.x ≈ re_sol.prob.x rtol=1e-2
+end
+
 include("../test_problems.jl")
